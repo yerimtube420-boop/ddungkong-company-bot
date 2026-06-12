89b136effb1578c61527ab14986fac8d51d97786
yerimtube420-boop <yerimtube420@gmail.com>
2026-06-12 14:00:43 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -390,7 +390,7 @@ def create_employee_card(user, xp, level, rank_name, join_date):
 
     card.paste(
         stamp,
-        (670, 390),
+        (650, 395),
         stamp
     )
     output = io.BytesIO()
